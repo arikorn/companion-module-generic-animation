7707373b2143d8f91a3ca0a5a8af24499b28e13a
arikorn <akmisc@natperspective.com>
2025-09-17 12:30:47 -0700

    Add internal functionality for "game playing" - copied from my conway repo, fixed up linting and other complaints - added empty controller.ts to replace to React App.tsx file.

---
+++ src/internal/conway.ts
@@ -0,0 +1,160 @@
+import { Grid, plus } from './grid.js'
+
+interface GoL_State {
+	board: Grid
+	nAlive: number
+}
+
+// Conway's Game Of Life
+export class GameOfLife {
+	// extends Grid
+	present: Grid // current generation
+	nAlive = 0
+	wrap = true
+
+	past: GoL_State[] = []
+	start: Grid
+
+	constructor(nrows: number, ncols: number) {
+		const newBoard = new Grid(nrows, ncols, 0)
+		this.present = newBoard
+		this.start = newBoard
+		this.nAlive = 0
+		this.wrap = true
+	}
+
+	setWrap(val: boolean): void {
+		this.wrap = val
+	}
+
+	// set a new start of game:
+	setBoard(newBoard: Grid): void {
+		this.clearHistory()
+		this.present = newBoard
+		this.nAlive = this.countAlive()
+		this.start = this.present
+	}
+
+	// restore the last start of game:
+	resetBoard(): void {
+		this.clearHistory()
+		this.present = this.start
+		this.nAlive = this.countAlive()
+	}
+
+	clear(): void {
+		this.clearHistory()
+		this.present = new Grid(this.present.nrow(), this.present.ncol(), 0)
+		this.nAlive = 0
+		//clear start?
+	}
+
+	countAlive(): number {
+		return this.present.reduceAll(plus)
+	}
+
+	updateHistory(): void {
+		// maintain two generations of history:
+		if (this.past.length >= 1) {
+			this.past[1] = this.past[0]
+		}
+		this.past[0] = { board: this.present, nAlive: this.nAlive }
+	}
+
+	clearHistory(): void {
+		this.past = []
+	}
+
+	// compute and return the next generation
+	next(): Grid {
+		// store the present state in history
+		this.updateHistory()
+
+		const source = this.present
+		const rows = source.nrow()
+		const nextgen = new Grid(rows, source.ncol(), 0)
+		const neighbors = nextgen.copy()
+
+		// compute neighbor values efficiently: for each "live" cell, increment all of its neighbors
+		for (let row = 0; row < rows; row++) {
+			const rowArray = source.getRow(row)
+			rowArray.forEach((value, col) => {
+				if (value > 0) {
+					this.markNeighbors(neighbors, row, col)
+				}
+			})
+		}
+
+		// populate the next generation matrix
+		// rules: 3 neighbors -- always alive
+		//            2 neighbors -- survive (i.e. if already alive)
+		//            all else -- dead
+		let nAlive = 0
+		for (let row = 0; row < rows; row++) {
+			const sourceRowArr = source.getRow(row)
+			const neighborRowArr = neighbors.getRow(row)
+			const nextgenRowArr = nextgen.getRow(row)
+
+			for (let col = 0; col < neighborRowArr.length; col++) {
+				const value = neighborRowArr[col]
+				if (value === 3) {
+					nextgenRowArr[col] = 1
+					nAlive++
+				} else if (value === 2) {
+					nextgenRowArr[col] = sourceRowArr[col]
+					nAlive += sourceRowArr[col]
+				} // everything else is zero
+			}
+		}
+
+		// TODO: Store current state, initial state
+		this.nAlive = nAlive
+		this.present = nextgen
+		return this.present
+	}
+
+	// Mark the eight neighbors of (row,col) by incrementing them by 1.
+	markNeighbors(dest: Grid, row: number, col: number): void {
+		const lastrow = dest.nrow() - 1
+		const lastcol = dest.ncol() - 1
+		const destdata = dest
+		const wrap = this.wrap
+		let xoffsets: number[]
+		let yoffsets: number[]
+
+		if (wrap) {
+			// offsets with wraparound: these will be added to row and col, so [-1, 0, 1] with adjustments
+			xoffsets = [row === 0 ? lastrow : -1, 0, row === lastrow ? -lastrow : 1]
+			yoffsets = [col === 0 ? lastcol : -1, 0, col === lastcol ? -lastcol : 1]
+		} else {
+			xoffsets = row === 0 ? [0, 1] : row === lastrow ? [-1, 0] : [-1, 0, 1]
+			yoffsets = col === 0 ? [0, 1] : col === lastcol ? [-1, 0] : [-1, 0, 1]
+		}
+		// increment each neighbor.
+		for (const xoff of xoffsets) {
+			for (const yoff of yoffsets) {
+				if (xoff === 0 && yoff === 0) continue
+				destdata[row + xoff][col + yoff]++
+			}
+		}
+	}
+
+	// return 0 if none are alive, -1 if no periodic cycle is found, otherwise return the period
+	//  currently we only detect period 1 (static) and period 2 (bi-phasic) cycles.
+	analyzeBoard(): number {
+		if (this.nAlive === 0) {
+			return 0
+		} // else
+		let period = 1
+		for (const bstate of this.past) {
+			if (bstate.nAlive === this.nAlive) {
+				// compare states.
+				if (this.present.equals(bstate.board)) {
+					return period
+				} // else
+				period++
+			}
+		}
+		return -1
+	}
+}
---
+++ src/internal/grid.ts
@@ -0,0 +1,199 @@
+// A grid or matrix object
+
+export interface Coord {
+	x: number
+	y: number
+}
+
+export enum Wipe {
+	Up, // defaults to 0
+	Down, // defaults to 1
+	Left, // defaults to 2
+	Right, // defaults to 3
+}
+
+export type BinaryFn = (a: number, b: number) => number
+
+export const plus: BinaryFn = (a, b) => a + b
+export const multiply: BinaryFn = (a, b) => a * b
+export const min: BinaryFn = (a, b) => Math.min(a, b)
+export const max: BinaryFn = (a, b) => Math.max(a, b)
+
+//export const equals:BinaryFn = (a, b) => a === b
+
+export class Grid extends Array<Array<number>> {
+	//data: number[][]
+
+	constructor(nrow: number, ncol: number, initval: number = 0) {
+		super(nrow)
+		for (let r = 0; r < nrow; r++) {
+			this[r] = new Array(ncol).fill(initval)
+		}
+	}
+
+	copy(): Grid {
+		const n = this.nrow()
+		const result = new Grid(this.nrow(), 0) // just need an empty object
+		for (let r = 0; r < n; r++) {
+			result[r] = this[r].slice()
+		}
+		return result
+	}
+
+	nrow(): number {
+		// rows are outer for now
+		return this.length
+	}
+
+	ncol(): number {
+		// rows are outer for now
+		return this.length > 0 ? this[0].length : 0
+	}
+
+	getRow(idx: number): number[] {
+		return this[idx]
+	}
+
+	val(idx: Coord): number {
+		return this[idx.x][idx.y]
+	}
+
+	set(idx: Coord, val: number): void {
+		this[idx.x][idx.y] = val
+	}
+
+	wipe(dir: Wipe, fromGrid: Grid | null = null, pos = 0): Grid {
+		switch (dir) {
+			case Wipe.Left:
+			case Wipe.Right:
+				return this.colShift(dir === Wipe.Left, fromGrid, pos)
+			case Wipe.Up:
+			case Wipe.Down:
+				return this.rowShift(dir === Wipe.Up, fromGrid, pos)
+		}
+	}
+
+	wrap(dir: Wipe): Grid {
+		switch (dir) {
+			case Wipe.Left:
+				return this.colShift(true, this, 0)
+			case Wipe.Right:
+				return this.colShift(false, this, this.ncol() - 1)
+			case Wipe.Up:
+				return this.rowShift(true, this, 0)
+			case Wipe.Down:
+				return this.rowShift(false, this, this.nrow() - 1)
+		}
+	}
+
+	//  shift up, possibly replacing with other data.
+	//   Note that rowShift(false, this, 0), for example, will wrap around
+	rowShift(up = false, newRow: number[] | Grid | null = null, newRowIdx = 0): Grid {
+		const result = this.copy()
+		if (newRow === null) {
+			newRow = new Array(this.ncol()).fill(0)
+		} else if (newRow instanceof Grid) {
+			if (newRow === this && newRowIdx === (up ? 0 : this.length - 1)) {
+				newRow = newRow[newRowIdx] // we're wrapping, just reuse discarded row
+			} else {
+				newRow = newRow[newRowIdx].slice() // make a copy, to be safe
+			}
+		}
+		if (up) {
+			result.shift() // remove first, then add 0 to end
+			result.push(newRow)
+		} else {
+			result.pop() // remove last, then add 0 to beginning
+			result.unshift(newRow)
+		}
+		return result
+	}
+
+	// shift left/right, replacing the removed element with either 0 or the specified new value
+	//   newCol has to have at least as many elements (if array) or rows (if Grid) as "this"
+	//   newColIdx is required if newCol is a Grid instance.
+	// Note that: colShift(true, this, 0), for example, will wrap around
+	colShift(left = false, newCol: number[] | Grid | null = null, newColIdx = 0): Grid {
+		const result = this.copy()
+		//  This works too, though it may produce more transient garbage??
+		// let newCol2:number[]|null
+
+		// if (newCol instanceof Grid) {
+		//     newCol2 = newCol.map(row => row[newColIdx]) // extract the column
+		// } else {
+		//     newCol2 = newCol
+		// }
+		if (left) {
+			result.forEach((row, idx) => {
+				// remove first, add 0 to end
+				row.shift()
+				row.push(newCol === null ? 0 : newCol instanceof Grid ? newCol[idx][newColIdx] : newCol[idx])
+			})
+		} else {
+			result.forEach((row, idx) => {
+				// remove last, add 0 to beginning
+				row.pop()
+				row.unshift(newCol === null ? 0 : newCol instanceof Grid ? newCol[idx][newColIdx] : newCol[idx])
+			})
+		}
+		return result
+	}
+
+	// note: toggle is not generalized: it will set the value to 0 or val (or 1 if val is missing)
+	//  coordinates are expressed as a nested array [[x1,y1,val1?], [x2,y2,val2?], ...]
+	//   "val" is optional and defaults to 1
+	setShape(idx: number[][], toggle = false, offset = [0, 0]): void {
+		idx.forEach((coord) => {
+			let row = coord[0] + offset[0]
+			let col = coord[1] + offset[1]
+			const val = coord.length > 2 ? coord[2] : 1
+			while (row + 1 > this.nrow()) {
+				// wrap values
+				row -= this.nrow()
+			}
+			while (row < 0) {
+				// wrap values
+				row += this.nrow()
+			}
+			while (col + 1 > this.ncol()) {
+				col -= this.ncol()
+			}
+			while (col < 0) {
+				col += this.ncol()
+			}
+			this[row][col] = toggle ? Number(!this[row][col]) * val : val
+		})
+	}
+
+	getShape(simplify = true): number[][] {
+		let result = []
+		for (let r = 0; r < this.length; r++) {
+			const row = this[r]
+			for (let c = 0; c < row.length; c++) {
+				if (row[c] > 0) result.push([r, c])
+			}
+		}
+		if (simplify && result.length > 0) {
+			const coord_min = result.reduce((gmin, val) => [Math.min(gmin[0], val[0]), Math.min(gmin[1], val[1])], result[0])
+			result = result.map((val) => [val[0] - coord_min[0], val[1] - coord_min[1]])
+		}
+		return result
+	}
+
+	reduceAll(op: (a: number, b: number) => number): number {
+		// for some reason, a nested reduce didn't work...
+		//const level1 = this.present.map(val => val.reduce((rowTotal, cellVal)=> rowTotal + cellVal, 0))
+		return this.reduce(
+			(total, rowArray) =>
+				op(
+					total,
+					rowArray.reduce((rowTotal, cellVal) => op(rowTotal, cellVal), 0),
+				),
+			0,
+		)
+	}
+
+	equals(b: Grid): boolean {
+		return this.every((rowArray, rowIdx) => rowArray.every((cellVal, colIdx) => cellVal === b[rowIdx][colIdx]))
+	}
+}

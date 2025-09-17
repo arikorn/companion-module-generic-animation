249dff60fe535794f8e560837cdf9e84337a2f6d
arikorn <akmisc@natperspective.com>
2025-09-17 12:28:42 -0700

    Initial feedback tests - read local variables (`$(this:row)`) - "advanced" feedback to set the grid

--- src/feedbacks.ts
+++ src/feedbacks.ts
@@ -1,31 +1,53 @@
 import { combineRgb } from '@companion-module/base'
 import type { LowresScreensaverInstance } from './main.js'
 
+export const on = '⬛'
+export const off = '⬜'
+export const dims = [10, 11] //[8, 9]
+
 export function UpdateFeedbacks(self: LowresScreensaverInstance): void {
 	self.setFeedbackDefinitions({
-		ChannelState: {
-			name: 'Example Feedback',
-			type: 'boolean',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-				color: combineRgb(0, 0, 0),
-			},
+		Text_X_Y: {
+			type: 'advanced',
+			name: 'Test local variable',
+			description: 'Change button text',
 			options: [
 				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 10,
+					id: 'X_Y',
+					type: 'dropdown',
+					label: 'X/Y',
+					choices: [{ id: '$(this:row),$(this:column)', label: 'Just this' }],
+					default: '$(this:row),$(this:column)',
 				},
 			],
-			callback: (feedback) => {
-				console.log('Hello world!', feedback.options.num)
-				if (Number(feedback.options.num) > 5) {
-					return true
-				} else {
-					return false
+			callback: async (feedback, context) => {
+				const controlY = Number(await context.parseVariablesInString('$(this:row)'))
+				const controlX = Number(await context.parseVariablesInString('$(this:column)'))
+				const opt = feedback.options.X_Y
+				return {
+					color: combineRgb(255, 255, 255),
+					bgcolor: combineRgb(72, 72, 72),
+					text: `row:${controlY} col:${controlX} ${controlY + controlX}\n${opt}`,
+				}
+			},
+		},
+		Conway: {
+			type: 'advanced',
+			name: 'Setup a Conway button',
+			description: 'Change button text',
+			options: [],
+			callback: async (_feedback) => {
+				//const controlY = Number(await context.parseVariablesInString('$(this:row)'))
+				//const controlX = Number(await context.parseVariablesInString('$(this:column)'))
+				const textVal = Array.from({ length: dims[0] }, () => new Array(dims[1]).fill(off)) //.join('\n')
+				for (let x = 0; x < dims[0]; x++) {
+					textVal[x][x] = on
+				}
+				return {
+					color: combineRgb(146, 146, 146),
+					bgcolor: combineRgb(0, 0, 0),
+					text: textVal.map((val) => val.join('')).join('\n'),
+					show_topbar: false,
 				}
 			},
 		},
